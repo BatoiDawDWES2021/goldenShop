8253f59d19df0afdba474d4732e656650a0f70c6
igomis <igomis@cipfpbatoi.es>
2020-09-19 08:38:09 +0200

    processPayment.php

--- public/processPayment.php
+++ public/processPayment.php
@@ -27,6 +27,9 @@ $errors = function() use ($shipping){
     if (empty($expirationDate) || date_create_from_format('m/y',$expirationDate) < new dateTime()) {
         $errors['expirationDate'] = "Expiration date must be in the future";
     }
+    if (empty($securityCode) || strlen($securityCode) != 3 || !is_numeric($securityCode))  {
+        $errors['securityCode'] = "Security Code must be 3 digits";
+    }
 
 
     if (is_uploaded_file($_FILES['dni']['tmp_name'])){
@@ -63,67 +66,75 @@ function classError($arrayErrors,$field){
             <div class="col-lg-9">
                 <?php
                 if ($_SERVER["REQUEST_METHOD"] == "POST" && count($errors()) === 0 ) {
-                    if (!move_uploaded_file($_FILES['dni']['tmp_name'], './dni/'.$_POST['cardNumber'].'.jpg')) {
+                    $destination = './dni/'.$_POST['cardNumber'].'.jpg';
+                    if (!move_uploaded_file($_FILES['dni']['tmp_name'],$destination )) {
                         echo "Error copying file";
                     } else {
                         echo "File upload";
+                        foreach ($_POST as $key => $value) {
+                            echo "<p><b>" . ucfirst($key) . "</b> : " . htmlspecialchars($value) . "</p>";
+                        }
+                        echo "<img src='$destination' title='foto dni' width='320' height='240'/>";
                     }
-                    foreach ($_POST as $key => $value) {
-                        echo "<p><b>" . ucfirst($key) . "</b> : " . htmlspecialchars($value) . "</p>";
-                    }
+
                 }
                 else {
+                    extract($_POST);
                 ?>
             <div class="container" >
                     <form action="processPayment.php" method="post" enctype="multipart/form-data">
                         <div class="form-control row">
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="typeCard" id="inlineRadio1" value="visa">
+                                <input class="form-check-input <?= classError($errors(),'typeCard') ?>" type="radio" name="typeCard" id="inlineRadio1" value="visa" <?php if ($typeCard === 'visa') echo 'checked' ?>>
                                 <label class="form-check-label" for="inlineRadio1">Visa</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="typeCard" id="inlineRadio2" value="mastercard">
+                                <input class="form-check-input <?= classError($errors(),'typeCard') ?>" type="radio" name="typeCard" id="inlineRadio2" value="mastercard" <?php if ($typeCard === 'mastercard') echo 'checked' ?>>
                                 <label class="form-check-label" for="inlineRadio2">Mastercard</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="typeCard" id="inlineRadio3" value="maestro">
+                                <input class="form-check-input <?= classError($errors(),'typeCard') ?>" type="radio" name="typeCard" id="inlineRadio3" value="maestro" <?php if ($typeCard === 'maestro') echo 'checked' ?>>
                                 <label class="form-check-label" for="inlineRadio3">Maestro</label>
+                                <?= showError($errors(),'typeCard') ?>
                             </div>
-
                         </div>
                         <div class="form-group row">
                             <label for="name" class="col-sm-3 col-form-label col-form-label-sm">Enter the CardHolder's Name:</label>
                             <div class="col-sm-3">
-                                <input type="text" class="form-control <?= classError($errors(),'name') ?>" id="name" name="name" placeholder="Enter your name">
+                                <input type="text" class="form-control <?= classError($errors(),'name') ?>" id="name" name="name" placeholder="Enter your name" value="<?= $name ?>">
                                 <?= showError($errors(),'name') ?>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="cardNumber" class="col-sm-3 col-form-label col-form-label-sm">Enter your Credit Card Number:</label>
                             <div class="col-sm-3">
-                                <input type="text" class="form-control <?= classError($errors(),'cardNumber') ?>" id="cardNumber" name="cardNumber" placeholder="Enter your creditCard Number">
+                                <input type="text" class="form-control <?= classError($errors(),'cardNumber') ?>" id="cardNumber" name="cardNumber" placeholder="Enter your creditCard Number" value="<?= $cardNumber ?>">
                                 <?= showError($errors(),'cardNumber') ?>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="expirationDate" class="col-sm-3 col-form-label col-form-label-sm">Enter Credit's Card expiration Date:</label>
                             <div class="col-sm-3">
-                                <input type="text" class="form-control <?= classError($errors(),'expirationDate') ?>" id="expirationDate" name="expirationDate" placeholder="Enter Credit's Card expiration Date">
+                                <input type="text" class="form-control <?= classError($errors(),'expirationDate') ?>" id="expirationDate" name="expirationDate" placeholder="Enter Credit's Card expiration Date" value="<?= $expirationDate ?>">
                                 <?= showError($errors(),'expirationDate') ?>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="securityCode" class="col-sm-3 col-form-label col-form-label-sm">Enter Security Code:</label>
                             <div class="col-sm-3">
-                                <input type="text" class="form-control <?= classError($errors(),'securityCode') ?>" id="securityCode" name="securityCode" placeholder="Enter Your Security Code">
+                                <input type="text" class="form-control <?= classError($errors(),'securityCode') ?>" id="securityCode" name="securityCode" placeholder="Enter Your Security Code" value="<?= $securityCode ?>">
                                 <?= showError($errors(),'securityCode') ?>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <div class="custom-file col-sm-3">
-                                <label class="custom-file-label" for="customFile">DNI picture</label>
-                                <input type="file" class="custom-file-input" name="dni" id="customFile">
-                                <?= showError($errors(),'dni') ?>
+                            <div class="input-group col-sm-6">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="dni" id="customFile">
+                                    <label class="custom-file-label " for="customFile">DNI picture</label>
+                                </div>
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-danger" type="button"><?= $errors()['dni']?></button>
+                                </div>
                             </div>
                         </div>
 
